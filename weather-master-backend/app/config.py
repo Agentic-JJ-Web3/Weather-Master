833cef45b2f853de44e1b06292240455c9426c69
Agentic-JJ-Web3 <njei.private@gmail.com>
2025-12-04 13:37:27 +0100

    4th commit

--- weather-master-backend/app/config.py
+++ weather-master-backend/app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     WEATHER_API_KEY: str
-    WEATHER_API_BASE_URL: str = "http://api.weatherapi.com/v1"
+    WEATHER_API_BASE_URL: str = "https://weatherapi.com/v1"
     CORS_ORIGINS: str = "http://localhost:3000,https://weather-master-three.vercel.app"
     GEMINI_API_KEY: str = "AIzaSyD5YYcbIviamb9w9y3xBX2eoQ8A-TtqTLI"
     
