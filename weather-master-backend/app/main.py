eec025ea0b27f32d0f763a56be4a6f2c20d47b99
Agentic-JJ-Web3 <njei.private@gmail.com>
2025-12-04 19:58:10 +0100

    Added CORS policy to allow request frpm my frontwnd

--- weather-master-backend/app/main.py
+++ weather-master-backend/app/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    Access-Control-Allow-Origin=settings.CORS_ORIGINS.split(",")
 )
 
 # Include routers
